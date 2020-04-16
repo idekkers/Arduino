61ff24b03804e66c2f1d6a27772e8adebbb379c8
idekkers <idekkers@hotmail.com>
2020-04-17 01:31:12 +0300

    arduino code

---
+++ Environment/Environment.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <ArduinoJson.h>
+
+// ---------BME280 setup-----------//
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+
+void setup()
+{
+  // Initialize a serial connection for reporting values to the host
+  Serial.begin(9600);
+
+  // BME280 temp humidity and
+  if (!bme.begin(0x76))
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1)
+      ;
+  }
+}
+
+void loop()
+{
+  //LM393 start analog light read
+  unsigned int AnalogValue;
+  AnalogValue = analogRead(A0);
+  DynamicJsonDocument json(1024);
+
+  //---------BME280-------------
+  json["environment"]["temp"] = bme.readTemperature();
+  json["environment"]["pressure"] = bme.readPressure() / 100.0F;
+  json["environment"]["humidity"] = bme.readHumidity();
+  //-----------LM393--------------
+  json["environment"]["light"] = AnalogValue;
+
+  Serial.println(serializeJson(json, Serial));
+  delay(15000);
+}
